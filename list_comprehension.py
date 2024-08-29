aedf3264ac04755c716f6bcfab14cd2bda1c9fe6
clintonjohn-cmd <johnouko@kabarak.ac.ke>
2024-08-29 13:19:18 +0300

    week4 project

---
+++ list_comprehension.py
@@ -0,0 +1,7 @@
+## list comprehension allows for creating a new list in a simple way , based on items that are already available in a single list.
+
+#EXAMPLE 1
+list1 = [23,76,43,87,98,54,32]
+double_numbers = [value*2 for value in list1]
+print(double_numbers)
+
